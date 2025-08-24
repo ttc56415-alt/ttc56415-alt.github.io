9c602090f964e6ee57bb6191f9888820e65c3e26
ttc56415-alt <ttc56415@ttc.ac.th>
2025-08-24 12:47:32 +0700

    Add files via upload

---
+++ index.html/1.js
@@ -0,0 +1,19 @@
+// 1.js
+document.getElementById("lang-toggle").addEventListener("click", function () {
+  const descTh = document.getElementById("desc-th");
+  const descEn = document.getElementById("desc-en");
+  const name = document.getElementById("restaurant-name");
+  const btn = document.getElementById("lang-toggle");
+
+  if (descTh.style.display !== "none") {
+    descTh.style.display = "none";
+    descEn.style.display = "block";
+    name.textContent = "Isan / Seafood Restaurant";
+    btn.textContent = "ภาษาไทย";
+  } else {
+    descTh.style.display = "block";
+    descEn.style.display = "none";
+    name.textContent = "ร้านอาหารอีสาน/ทะเลซีฟู้ด";
+    btn.textContent = "English";
+  }
+});
